024253880cc2b984ef369f1c7b67f3c2bc9223d7
Matthew Neavling <matthew.neavling@proton.me>
2025-08-22 11:31:22 -0400

    create FastTranscript class; remove click

--- src/azure_fast_transcript/__init__.py
+++ src/azure_fast_transcript/__init__.py
@@ -1,7 +1,8 @@
-from typing import TypedDict, Generator
+from typing import Literal, TypedDict, Generator
 from datetime import time
 import os
 import json
+import logging
 
 import requests
 
@@ -9,17 +10,22 @@ DEFAULT_ENDPOINT = "https://eastus.api.cognitive.microsoft.com/speechtotext/tran
 MAX_LINE_LENGTH = 25
 MAX_LINES = 2
 
+logger = logging.getLogger()
+logging.basicConfig()
+
+ProfanityFilter = Literal["None", "Masked", "Removed", "Tags"]
+
 
 class Word(TypedDict):
     text: str
-    offset: int
-    duration: int
+    offsetMilliseconds: int
+    durationMilliseconds: int
 
 
 class Phrase(TypedDict):
     channel: int
-    offset: int
-    duration: int
+    offsetMilliseconds: int
+    durationMilliseconds: int
     text: str
     words: list[Word]
     locale: str
@@ -31,16 +37,110 @@ class Channel(TypedDict):
     text: str
 
 
-class FastTranscript(TypedDict):
-    duration: int
-    combinedPhrases: list[Channel]
-    phrases: list[Phrase]
-
-
 class CaptionLine(TypedDict):
     start: int
     end: int
     text: str
+
+
+def _get_line(
+    words_list: list[Word],
+    max_line_length: int = MAX_LINE_LENGTH,
+    max_lines: int = MAX_LINES,
+) -> Generator[CaptionLine, None, None]:
+    while len(words_list) > 0:
+        candidate_words: list[Word] = []
+        lines = []
+        while len(lines) < max_lines:
+            line = ""
+            while len(line) < max_line_length:
+                try:
+                    new_word = words_list.pop(0)
+                    line += new_word["text"] + " "
+                    candidate_words.append(new_word)
+                except IndexError:
+                    break
+            if line:
+                lines.append(line)
+            else:
+                break
+
+        start = candidate_words[0]["offsetMilliseconds"]
+        end = (
+            candidate_words[-1]["offsetMilliseconds"] + candidate_words[-1]["durationMilliseconds"]
+        )
+        text = "\n".join(lines)
+        yield {"start": start, "end": end, "text": text}
+
+
+def _time_from_milliseconds(milliseconds: int) -> str:
+    seconds = milliseconds / 1000
+    microsecond = (milliseconds * 1000) % 10**6
+    second = int(seconds) % 60
+    minute = int(seconds / 60) % 60
+    ts = time(minute=minute, second=second, microsecond=microsecond)
+
+    return "{:02}:{:02}:{:02}.{:03}".format(
+        ts.hour, ts.minute, ts.second, ts.microsecond // 1000
+    )
+
+
+class FastTranscript:
+    duration: int
+    combined_phrases: list[Channel]
+    phrases: list[Phrase]
+
+    def __init__(
+        self, duration: int, combined_phrases: list[Channel], phrases: list[Phrase]
+    ) -> None:
+        self.duration = duration
+        self.combined_phrases = combined_phrases
+        self.phrases = phrases
+
+    def to_vtt(self) -> str:
+        """Return VTT subtitles"""
+        enumerator = 1
+
+        vtt = "WEBVTT"
+        vtt += "\n"
+        for phrase in self.phrases:
+            words = phrase["words"]
+
+            lines = _get_line(words)
+            while True:
+                try:
+                    next_line = next(lines)
+                    start = next_line["start"]
+                    end = next_line["end"]
+                    vtt += f"\n{enumerator}"
+                    vtt += f"\n{_time_from_milliseconds(start)} --> {_time_from_milliseconds(end)}"
+                    vtt += f"\n{next_line['text']}"
+                    vtt += "\n"
+                    enumerator += 1
+                except StopIteration:
+                    break
+
+        return vtt
+
+    def to_raw(self) -> str:
+        """Return raw transcript"""
+        channels = self.combined_phrases
+        if len(channels) > 1:
+            # handle multiple speakers
+            return "\n".join(t["text"] for t in channels)
+        else:
+            return channels[0]["text"]
+
+    @staticmethod
+    def from_object(data: dict) -> "FastTranscript":
+        """JSON decoder hook
+        :param data: dictionary to parse as FastTranscript
+        :returns: FastTranscript
+        """
+        logger.debug(f"Input object to FastTranscript.from_object: {data}")
+        return FastTranscript(
+            data["durationMilliseconds"], data["combinedPhrases"], data["phrases"]
+        )
 
 
 class Transcript:
@@ -49,25 +149,29 @@ class Transcript:
         :param file: file to transcribe
         :param endpoint: Fast transcription API endpoint
         """
-        # Init with endpoint and file
         self.endpoint = endpoint
         self.file = file
         self.transcript = {}
 
-    def get_transcript(self) -> FastTranscript:
+    def get_transcript(
+        self, locales: list[str] = ["en-US"], profanity_filter: ProfanityFilter = "None"
+    ) -> FastTranscript:
         """POST file to endpoint
-        :returns:
+        :returns: FastTranscript
         """
 
         subscription_key = os.environ.get("SPEECH_KEY")
         if not subscription_key:
+            logger.fatal("SPEECH_KEY missing from environment.")
             raise Exception("SPEECH_KEY missing from environment.")
 
         headers = {
             "Accept": "application/json",
             "Ocp-Apim-Subscription-Key": subscription_key,
         }
-        definition = json.dumps({"locales": ["en-US"], "profanityFilter": "None"})
+        definition = json.dumps(
+            {"locales": locales, "profanityFilter": profanity_filter}
+        )
         files = {
             "definition": (None, definition, "application/json"),
             "audio": (
@@ -77,95 +181,44 @@ class Transcript:
             ),
         }
 
-        resp = requests.post(self.endpoint, headers=headers, files=files)
+        session = requests.Session()
+        req = requests.Request("POST", url=self.endpoint, headers=headers, files=files)
+        req = session.prepare_request(req)
+        logger.debug(req)
+
+        # resp = requests.post(self.endpoint, headers=headers, files=files)
+        resp = session.send(req)
         if resp.ok:
-            return resp.json()
+            resp_body = resp.json()
+            logger.debug(resp_body)
+            return FastTranscript.from_object(resp_body)
         else:
-            raise Exception(
-                f"Response type invalid:\nCode:\t{resp.status_code}\nMsg:\t{resp.content}"
-            )
+            err = f"Response type invalid:\nCode:\t{resp.status_code}\nMsg:\t{resp.content}"
+            logger.fatal(err)
+            raise Exception(err)
 
-    def _get_line(
-        self,
-        words_list: list[Word],
-        max_line_length: int = MAX_LINE_LENGTH,
-        max_lines: int = MAX_LINES,
-    ) -> Generator[CaptionLine, None, None]:
-        while len(words_list) > 0:
-            candidate_words: list[Word] = []
-            lines = []
-            while len(lines) < max_lines:
-                line = ""
-                while len(line) < max_line_length:
-                    try:
-                        new_word = words_list.pop(0)
-                        line += new_word["text"] + " "
-                        candidate_words.append(new_word)
-                    except IndexError:
-                        break
-                if line:
-                    lines.append(line)
-                else:
-                    break
-
-            start = candidate_words[0]["offset"]
-            end = candidate_words[-1]["offset"] + candidate_words[-1]["duration"]
-            text = "\n".join(lines)
-            yield {"start": start, "end": end, "text": text}
-
-    def _time_from_milliseconds(self, milliseconds: int) -> str:
-        seconds = milliseconds / 1000
-        microsecond = (milliseconds * 1000) % 10**6
-        second = int(seconds) % 60
-        minute = int(seconds / 60) % 60
-        ts = time(minute=minute, second=second, microsecond=microsecond)
-
-        return "{:02}:{:02}:{:02}.{:03}".format(
-            ts.hour, ts.minute, ts.second, ts.microsecond // 1000
+    @staticmethod
+    def get_subtitles(
+        file,
+        locales: list[str] = ["en-US"],
+        profanity_filter: ProfanityFilter = "None",
+        endpoint: str = DEFAULT_ENDPOINT,
+    ) -> str:
+        transcript = Transcript(file=file, endpoint=endpoint)
+        response = transcript.get_transcript(
+            locales=locales, profanity_filter=profanity_filter
         )
-
-    def parse_subtitles(self, response: FastTranscript) -> str:
-        enumerator = 1
-
-        vtt = "WEBVTT"
-        vtt += "\n"
-        for phrase in response["phrases"]:
-            words = phrase["words"]
-
-            lines = self._get_line(words)
-            while True:
-                try:
-                    next_line = next(lines)
-                    start = next_line["start"]
-                    end = next_line["end"]
-                    vtt += f"\n{enumerator}"
-                    vtt += f"\n{self._time_from_milliseconds(start)} --> {self._time_from_milliseconds(end)}"
-                    vtt += f"\n{next_line['text']}"
-                    vtt += "\n"
-                    enumerator += 1
-                except StopIteration:
-                    break
-
-        return vtt
-
-    def parse_transcript(self, response: FastTranscript) -> str:
-        channels = response["combinedPhrases"]
-        if len(channels) > 1:
-            # handle multiple speakers
-            return "\n".join(t["text"] for t in channels)
-        else:
-            return channels[0]["text"]
+        return response.to_vtt()
 
     @staticmethod
-    def get_subtitles(file, endpoint: str = DEFAULT_ENDPOINT) -> str:
+    def get_raw(
+        file,
+        locales: list[str] = ["en-US"],
+        profanity_filter: ProfanityFilter = "None",
+        endpoint: str = DEFAULT_ENDPOINT,
+    ) -> str:
         transcript = Transcript(file=file, endpoint=endpoint)
-        response = transcript.get_transcript()
-        subtitles = transcript.parse_subtitles(response)
-        return subtitles
-
-    @staticmethod
-    def get_raw(file, endpoint: str = DEFAULT_ENDPOINT) -> str:
-        transcript = Transcript(file=file, endpoint=endpoint)
-        response = transcript.get_transcript()
-        transcript = transcript.parse_transcript(response)
-        return transcript
+        response = transcript.get_transcript(
+            locales=locales, profanity_filter=profanity_filter
+        )
+        return response.to_raw()
